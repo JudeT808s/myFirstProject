f867749062c06337fecbda1a6ac18d46edcc5294
jude <n00213344@iadt.ie>
2022-09-30 12:00:35 +0100

    second_commit

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Introduction</title>
+</head>
+<body>
+    <h1>Hello {{    $name   }}</hl>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis asperiores voluptate ipsa nihil ab voluptatibus non corporis, ipsum magni eos!</p>
+</body>
+</html>
